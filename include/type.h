821674baac7c66142cb5134b1067d8db31d0dc0a
fengjiran <fengjiran@foxmail.com>
2025-08-13 22:54:54 +0800

    add type

---
+++ include/type.h
@@ -0,0 +1,62 @@
+//
+// Created by 赵丹 on 2025/8/13.
+//
+
+#ifndef AETHERMIND_TYPE_H
+#define AETHERMIND_TYPE_H
+
+#include "macros.h"
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <utility>
+
+namespace aethermind {
+
+#define FORALL_TYPES(_) \
+    _(AnyType)          \
+    _(EnumType)         \
+    _(AnyEnumType)      \
+    _(TensorType)       \
+    _(StorageType)      \
+    _(TupleType)        \
+    _(ListType)         \
+    _(DictType)         \
+    _(NumberType)       \
+    _(FloatType)        \
+    _(ComplexType)      \
+    _(FutureType)       \
+    _(AwaitType)        \
+    _(RRefType)         \
+    _(IntType)          \
+    _(NoneType)         \
+    _(StringType)       \
+    _(GeneratorType)    \
+    _(QuantizerType)    \
+    _(BoolType)         \
+    _(OptionalType)     \
+    _(VarType)          \
+    _(DeviceObjType)    \
+    _(StreamObjType)    \
+    _(FunctionType)     \
+    _(ClassType)        \
+    _(PyObjectType)     \
+    _(CapsuleType)      \
+    _(InterfaceType)    \
+    _(QSchemeType)      \
+    _(ScalarTypeType)   \
+    _(LayoutType)       \
+    _(MemoryFormatType) \
+    _(AnyListType)      \
+    _(AnyTupleType)     \
+    _(AnyClassType)     \
+    _(SymIntType)       \
+    _(SymFloatType)     \
+    _(SymBoolType)      \
+    _(UnionType)        \
+    _(DynamicType)
+
+}// namespace aethermind
+
+#endif//AETHERMIND_TYPE_H
---
+++ src/type.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 赵丹 on 2025/8/13.
+//
+
+#include "type.h"
+
+namespace aethermind {
+
+
+
+}

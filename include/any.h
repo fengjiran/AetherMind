6fdaf053b56eef8dc8a52356b6c0bfad73e278d3
fengjiran <fengjiran@foxmail.com>
2025-08-16 16:03:13 +0800

    add type

--- include/any.h
+++ include/any.h
@@ -11,73 +11,12 @@
 
 namespace aethermind {
 
-#define AETHERMIND_FORALL_TAGS(_) \
-    _(None)                       \
-    _(Tensor)                     \
-    _(Storage)                    \
-    _(Double)                     \
-    _(ComplexDouble)              \
-    _(Int)                        \
-    _(SymInt)                     \
-    _(SymFloat)                   \
-    _(SymBool)                    \
-    _(Bool)                       \
-    _(Tuple)                      \
-    _(String)                     \
-    _(Blob)                       \
-    _(GenericList)                \
-    _(GenericDict)                \
-    _(Future)                     \
-    _(Await)                      \
-    _(Device)                     \
-    _(Stream)                     \
-    _(Object)                     \
-    _(PyObject)                   \
-    _(Uninitialized)              \
-    _(Capsule)                    \
-    _(RRef)                       \
-    _(Quantizer)                  \
-    _(Generator)                  \
-    _(Enum)
-
-enum class Tag : uint32_t {
-#define DEFINE_TAG(x) x,
-    AETHERMIND_FORALL_TAGS(DEFINE_TAG)
-#undef DEFINE_TAG
-};
-
-union Payload {
-    union data {
-        int64_t v_int;
-        double v_double;
-        bool v_bool;
-        const char* v_str;
-        void* v_handle;
-        Device v_device;
-
-        data() : v_int(0) {}
-    } u;
-
-    Tensor v_tensor;
-
-    static_assert(std::is_trivially_copyable_v<data>);
-    Payload() : u() {}
-    Payload(const Payload&) = delete;
-    Payload(Payload&&) = delete;
-    Payload& operator=(const Payload&) = delete;
-    Payload& operator=(Payload&&) = delete;
-
-    ~Payload() {}
-};
-
-
 class Any {
 public:
     Any() = default;
 
 private:
-    Payload payload_;
-    Tag tag_;
+    AetherMindAny data_;
 
 #define COUNT_TAG(x) 1 +
     static constexpr auto kNumTags = AETHERMIND_FORALL_TAGS(COUNT_TAG) 0;
--- include/type_traits.h
+++ include/type_traits.h
@@ -11,11 +11,146 @@
 
 namespace aethermind {
 
+#define AETHERMIND_FORALL_TAGS(_) \
+    _(None)                       \
+    _(Tensor)                     \
+    _(Storage)                    \
+    _(Double)                     \
+    _(ComplexDouble)              \
+    _(Int)                        \
+    _(SymInt)                     \
+    _(SymFloat)                   \
+    _(SymBool)                    \
+    _(Bool)                       \
+    _(Tuple)                      \
+    _(String)                     \
+    _(Blob)                       \
+    _(GenericList)                \
+    _(GenericDict)                \
+    _(Future)                     \
+    _(Await)                      \
+    _(Device)                     \
+    _(Stream)                     \
+    _(Object)                     \
+    _(PyObject)                   \
+    _(Uninitialized)              \
+    _(Capsule)                    \
+    _(RRef)                       \
+    _(Quantizer)                  \
+    _(Generator)                  \
+    _(Enum)
+
+enum class Tag : uint32_t {
+#define DEFINE_TAG(x) x,
+    AETHERMIND_FORALL_TAGS(DEFINE_TAG)
+#undef DEFINE_TAG
+};
+
+struct AetherMindAny {
+    union Payload {
+        union data {
+            int64_t v_int;
+            double v_double;
+            bool v_bool;
+            const char* v_str;
+            void* v_handle;
+            Device v_device;
+
+            data() : v_int(0) {}
+        };
+
+        data u;
+        Tensor v_tensor;
+
+        static_assert(std::is_trivially_copyable_v<data>);
+        Payload() : u() {}
+        Payload(const Payload&) = delete;
+        Payload(Payload&&) = delete;
+        Payload& operator=(const Payload&) = delete;
+        Payload& operator=(Payload&&) = delete;
+
+        ~Payload() {}
+    };
+
+    Payload payload_;
+    Tag tag_ {Tag::None};
+};
+
+
 template<typename, typename = void>
 struct TypeTraits;
 
+template<>
+struct TypeTraits<bool> {
+    static void CopyToAny(const bool& src, AetherMindAny* dst) {
+        dst->tag_ = Tag::Bool;
+        dst->payload_.u.v_bool = src;
+    }
 
+    static void MoveToAny(bool src, AetherMindAny* dst) {
+        CopyToAny(src, dst);
+    }
 
-}
+    static bool CopyFromAnyAfterCheck(const AetherMindAny* src) {
+        return src->payload_.u.v_bool;
+    }
+
+    static bool MoveFromAnyAfterCheck(AetherMindAny* src) {
+        return src->payload_.u.v_bool;
+    }
+
+    static std::optional<bool> TryCastFromAny(const AetherMindAny* src) {
+        if (check(src)) {
+            return src->payload_.u.v_bool;
+        }
+        return std::nullopt;
+    }
+
+    static bool check(const AetherMindAny* src) {
+        return src->tag_ == Tag::Bool;
+    }
+
+    static std::string TypeStr() {
+        return "bool";
+    }
+};
+
+// POD Int type
+template<typename T>
+struct TypeTraits<T, std::enable_if_t<std::is_integral_v<T>>> {
+    static void CopyToAny(const T& src, AetherMindAny* dst) {
+        dst->tag_ = Tag::Int;
+        dst->payload_.u.v_int = static_cast<int64_t>(src);
+    }
+
+    static void MoveToAny(T src, AetherMindAny* dst) {
+        CopyToAny(src, dst);
+    }
+
+    static T CopyFromAnyAfterCheck(const AetherMindAny* src) {
+        return src->payload_.u.v_int;
+    }
+
+    static T MoveFromAnyAfterCheck(AetherMindAny* src) {
+        return src->payload_.u.v_int;
+    }
+
+    static std::optional<T> TryCastFromAny(const AetherMindAny* src) {
+        if (check(src)) {
+            return static_cast<T>(src->payload_.u.v_int);
+        }
+        return std::nullopt;
+    }
+
+    static bool check(const AetherMindAny* src) {
+        return src->tag_ == Tag::Int;
+    }
+
+    static std::string TypeStr() {
+        return "int";
+    }
+};
+
+}// namespace aethermind
 
 #endif//AETHERMIND_TYPE_TRAITS_H

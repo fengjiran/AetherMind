4896ae59af27d1adcf41f83c2ccf984e05374fd6
fengjiran <fengjiran@foxmail.com>
2025-08-16 08:32:25 +0800

    add device

--- include/any.h
+++ include/any.h
@@ -10,17 +10,60 @@
 
 namespace aethermind {
 
+#define AETHERMIND_FORALL_TAGS(_) \
+    _(None)                       \
+    _(Tensor)                     \
+    _(Storage)                    \
+    _(Double)                     \
+    _(ComplexDouble)              \
+    _(Int)                        \
+    _(SymInt)                     \
+    _(SymFloat)                   \
+    _(SymBool)                    \
+    _(Bool)                       \
+    _(Tuple)                      \
+    _(String)                     \
+    _(Blob)                       \
+    _(GenericList)                \
+    _(GenericDict)                \
+    _(Future)                     \
+    _(Await)                      \
+    _(Device)                     \
+    _(Stream)                     \
+    _(Object)                     \
+    _(PyObject)                   \
+    _(Uninitialized)              \
+    _(Capsule)                    \
+    _(RRef)                       \
+    _(Quantizer)                  \
+    _(Generator)                  \
+    _(Enum)
+
+enum class Tag : uint32_t {
+#define DEFINE_TAG(x) x,
+    AETHERMIND_FORALL_TAGS(DEFINE_TAG)
+#undef DEFINE_TAG
+};
+
+
+
 class Any {
 public:
+
+
 private:
     union data {
-        int64_t v_int;
-        double v_float;
+        int64_t v_int{};
+        double v_double;
         bool v_bool;
         Device v_device;
         Tensor v_tensor;
         void* v_handle;
+
+        data() : v_int(0) {}
     } data_;
+
+    Tag tag_;
 };
 
 }// namespace aethermind

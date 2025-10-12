d1b8c93cbf57097b479fa4fc4be827b7025bdd93
fengjiran <fengjiran@foxmail.com>
2025-10-12 22:52:31 +0800

    add memory format

--- include/memory_format.h
+++ include/memory_format.h
@@ -28,15 +28,59 @@ namespace aethermind {
 //  ChannelsLast:
 //    Regardless of input tensors format, the output should be in channels_last
 //    format.
+// https://juejin.cn/post/7234454112955580475
 enum class MemoryFormat : uint8_t {
+    // NCHW, dense, no overlap,
+    // strides[0] > strides[1] > strides[2] > strides[3] == 1
     Contiguous,
     Preserve,
+    // NHWC, dense, no overlap
+    // strides[0] > strides[2] > strides[3] > strides[1] == 1
     ChannelsLast,
-    ChannelsLast3d,
+    ChannelsLast3d,// NDHWC
     NumOptions
 };
 
 inline MemoryFormat get_contiguous_memory_format() {
+    return MemoryFormat::Contiguous;
+}
+
+template<typename T>
+std::vector<T> get_channels_last_strides_2d(ArrayView<T> shape) {
+    std::vector<T> strides(shape.size());
+    switch (shape.size()) {
+        case 4: {
+            strides[1] = 1;
+            strides[3] = shape[1];
+            strides[2] = strides[3] * shape[3];
+            strides[0] = strides[2] * shape[2];
+        }
+        case 3: {
+            strides[0] = 1;
+            strides[2] = shape[0];
+            strides[1] = strides[2] * shape[2];
+        }
+        default: {
+            CHECK(false) << "ChannelsLast2d doesn't support size " << shape.size();
+        }
+    }
+    return strides;
+}
+
+inline std::ostream& operator<<(std::ostream& os, MemoryFormat format) {
+    switch (format) {
+        case MemoryFormat::Preserve:
+            os << "Preserve";
+        case MemoryFormat::Contiguous:
+            os << "Contiguous";
+        case MemoryFormat::ChannelsLast:
+            os << "ChannelsLast";
+        case MemoryFormat::ChannelsLast3d:
+            os << "ChannelsLast3d";
+        default:
+            CHECK(false) << "Unknown memory format.";
+    }
+    return os;
 }
 
 }// namespace aethermind

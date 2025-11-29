3ef768120d20813d9f43697eb0c11d4eddc40e11
fengjiran <fengjiran@foxmail.com>
2025-11-29 21:56:19 +0800

    add flat map

--- include/container/map.h
+++ include/container/map.h
@@ -12,6 +12,31 @@
 
 namespace aethermind {
 
+struct Item {
+    std::pair<Any, Any> data;
+    size_t prev = std::numeric_limits<size_t>::max();
+    size_t next = std::numeric_limits<size_t>::max();
+};
+
+struct Block {
+    uint8_t bytes[16 + 16 * sizeof(Item)];
+
+    Block() {
+        auto* p = reinterpret_cast<Item*>(bytes + 16);
+        for (int i = 0; i < 16; ++i) {
+            bytes[i] = 0xFF;
+            new (p++) Item();
+        }
+    }
+
+    ~Block() {
+        auto* p = reinterpret_cast<Item*>(bytes + 16);
+        for (int i = 0; i < 16; ++i) {
+            p->~Item();
+        }
+    }
+};
+
 class MapImpl : public Object {
 public:
     using key_type = Any;
@@ -20,23 +45,62 @@ public:
 
     static_assert(sizeof(KVType) == 16);
 
+    class iterator;
+
     MapImpl() : data_(nullptr), size_(0), slot_(0) {}
 
-    size_t size() const {
+    NODISCARD size_t size() const {
         return size_;
     }
 
-private:
+protected:
     void* data_;
     size_t size_;
     size_t slot_;
+
+    // Small map mask, the most significant bit is used to indicate the small map layout.
+    static constexpr size_t kSmallMapMask = static_cast<size_t>(1) << 63;
+
+    bool IsSmallMap() const {
+        return (slot_ & kSmallMapMask) != 0ull;
+    }
+};
+
+class SmallMapImpl : public MapImpl {
+public:
+    using MapImpl::iterator;
+
+    size_t GetSlotNum() const {
+        return slot_ & ~kSmallMapMask;
+    }
+
+private:
+    static constexpr size_t kInitSize = 2;
+    static constexpr size_t kMaxSize = 4;
+
+    static ObjectPtr<SmallMapImpl> Create(size_t n = kInitSize);
+
+    template<typename Iter>
+    static ObjectPtr<SmallMapImpl> CreateFromRange(size_t n, Iter first, Iter last) {
+        ObjectPtr<SmallMapImpl> impl = Create(n);
+        auto* ptr = static_cast<KVType*>(impl->data_);
+        while (first != last) {
+            new (ptr++) KVType(*first++);
+        }
+        return impl;
+    }
+
+    static ObjectPtr<SmallMapImpl> CopyFrom(const SmallMapImpl* src);
+};
+
+class DenseMapImpl : public MapImpl {
 };
 
 template<typename K, typename V>
 class Map : public ObjectRef {
 public:
 private:
-
+    ObjectPtr<MapImpl> impl_;
 };
 
 }// namespace aethermind
--- src/container/map.cpp
+++ src/container/map.cpp
@@ -6,6 +6,51 @@
 
 namespace aethermind {
 
+class MapImpl::iterator {
+public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = KVType;
+    using pointer = value_type*;
+    using reference = value_type&;
+    using difference_type = int64_t;
 
+    iterator() : index_(0), ptr_(nullptr) {}
 
+    // pointer operator->() const {
+    //     //
+    // }
+
+    bool operator==(const iterator& other) const {
+        return index_ == other.index_ && ptr_ == other.ptr_;
+    }
+
+    bool operator!=(const iterator& other) const {
+        return !(*this == other);
+    }
+
+protected:
+    iterator(size_t index, const MapImpl* ptr) : index_(index), ptr_(ptr) {}
+
+    size_t index_;      // The position in the array.
+    const MapImpl* ptr_;// The container it pointer to.
+
+    friend class SmallMapImpl;
+    friend class DenseMapImpl;
+};
+
+ObjectPtr<SmallMapImpl> SmallMapImpl::Create(size_t n) {
+    auto impl = make_array_object<SmallMapImpl, KVType>(n);
+    impl->data_ = reinterpret_cast<char*>(impl.get()) + sizeof(SmallMapImpl);
+    impl->size_ = 0;
+    impl->slot_ = n & ~kSmallMapMask | kSmallMapMask;
+    return impl;
 }
+
+ObjectPtr<SmallMapImpl> SmallMapImpl::CopyFrom(const SmallMapImpl* src) {
+    auto* first = static_cast<KVType*>(src->data_);
+    auto* last = first + src->size_;
+    return CreateFromRange(src->size_, first, last);
+}
+
+
+}// namespace aethermind
